00ecf9938ce1ba4edb3a448a1e1a7178c799c25d
Dragusin Cristian <dragusincristian609@gmail.com>
2026-09-02 17:33:54 +0300

    students hashmap

---
+++ src/entities/Student.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Student {
+    private Integer id;
+    private String name;
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    };
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
