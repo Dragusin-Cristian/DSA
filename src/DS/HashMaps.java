a8d3b08f9f2126d0e4821cdc9da969c83dc92bda
Dragusin Cristian <dragusincristian609@gmail.com>
2026-09-02 17:45:04 +0300

    DS package structure

---
+++ src/DS/HashMaps.java
@@ -0,0 +1,26 @@
+package DS;
+
+import entities.Student;
+
+import java.util.HashMap;
+
+public class HashMaps {
+
+    public static void example() {
+        Student[] students =  new Student[3];
+        students[0] = new Student(0, "Mihai");
+        students[1] = new Student(1, "Cristi");
+        students[2] = new Student(2, "Andrei");
+
+        System.out.println(HashMaps.buildStudentsMap(students).toString());
+    }
+
+    static HashMap<Integer, String> buildStudentsMap(Student[] students) {
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        for (Student student : students) {
+            map.put(student.getId(), student.getName());
+        }
+        return map;
+    };
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,11 @@
-import entities.Student;
-
-import java.util.HashMap;
+import DS.HashMaps;
 
 public class Main {
     public static void main(String[] args) {
 
-        Student[] students =  new Student[3];
-        students[0] = new Student(0, "Mihai");
-        students[1] = new Student(1, "Cristi");
-        students[2] = new Student(2, "Andrei");
+        HashMaps.example();
 
-        System.out.println(Main.buildStudentsMap(students).toString());
     }
 
-    public static HashMap<Integer, String> buildStudentsMap(Student[] students) {
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
-        for (Student student : students) {
-            map.put(student.getId(), student.getName());
-        }
-        return map;
-    };
 
 }
